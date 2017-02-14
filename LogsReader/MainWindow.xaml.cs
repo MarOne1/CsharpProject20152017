91b001dc29d626dc898de286bc3c362197aefed4
Marouane Benabed <benabedmarouane@outlook.com>
2017-02-14 11:28:06 +0100

    ADD 2 buttons, one for upload and the second for export, will start with uploading text from a text file

--- LogsReader/MainWindow.xaml.cs
+++ LogsReader/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace LogsReader
 {
@@ -20,9 +22,44 @@ namespace LogsReader
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string filename;
+        private string filecontent;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+
+        /*Lire le fichier texte*/
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog boiteDialogue1 = new OpenFileDialog();
+            boiteDialogue1.Multiselect = false;
+            boiteDialogue1.Filter = "Fichiers txt |*.txt";
+            boiteDialogue1.FilterIndex = 2;
+            boiteDialogue1.RestoreDirectory = true;
+
+            try
+            {
+                using(Stream stream1 = boiteDialogue1.OpenFile())
+                {
+                    using (StreamReader reader = new StreamReader(stream1, Encoding.UTF8))
+                    {
+                        filecontent = reader.ReadToEnd();
+                    }
+
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
